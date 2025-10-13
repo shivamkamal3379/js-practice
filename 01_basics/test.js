1e7af99b1c7c09d0c7ff67fd375105508ffef753
shivam kamal <shivamkamal3379@gmail.com>
2025-10-13 08:08:50 +0530

    Update test.js

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,30 @@
 console.log("shivam bhaii");
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be highin this case 
+
+console.log("shivam")
+
+
+let name = "shivam_kamal"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2  raise to the  power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //  it is object
